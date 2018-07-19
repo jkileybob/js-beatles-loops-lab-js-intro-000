5a0420471b13d3fcf100eb775623c20bacefa4f6
Kiley Bobbitt <jkileybob@aol.com>
2018-07-19 16:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments){
-  const musicians = ["John", "Paul", "George", "Ringo"]
-  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
   var array = [];
   for (var i = 0; i < musicians.length; i++); {
     array.push(musicians[i] + " plays " + instruments[i]);
