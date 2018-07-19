4e68648d5434837a93787ce0a9a368ca80199c68
Kiley Bobbitt <jkileybob@aol.com>
2018-07-19 16:57:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function theBeatlesPlay(musicians, instruments){
     }
     return array
 }
+
+function johnLennonFacts(array){
+  var i = 0
+  while (i < array.length){
+    array[i] + "!!!"; i++
+  }
+  return array
+}
